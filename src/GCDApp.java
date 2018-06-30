58a8815ef2c81564faa35cd56dd5f2c289a1955a
Unknown <econcelman@gmail.com>
2018-06-30 17:12:18 -0400

    Adding the user input part

--- src/GCDApp.java
+++ src/GCDApp.java
@@ -1,10 +1,20 @@
-
+import java.util.Scanner;
 public class GCDApp {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int d, r;
+		
+		System.out.println("Please enter the first number: ");
+		d = scan.nextInt();
+		System.out.println();
+		
+		System.out.println("Please enter the second number: ");
+		r = scan.nextInt();
+		System.out.println();
 		
 		
-		System.out.println(gcd(18,5));
+		System.out.println("Greatest common denominator between " + d + " and " + r + " is " + gcd(d,r));
 			
 		}
 
